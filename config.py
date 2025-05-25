8931103233ee68269c22e71d503935ddfd8a64c3
김수윤 <suyunkim@gimsuyun-ui-MacBookAir.local>
2025-05-25 17:26:13 +0900

    Stop tracking config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-model_name = "skt/kogpt2-base-v2"
-max_length = 512
-batch_size = 4
-epochs = 5
-lr = 5e-5
-save_path = "./checkpoint"
-data_path = "./data/poems.txt"
-tokenized_data_path = "./data/tokenized_dataset.pt"
